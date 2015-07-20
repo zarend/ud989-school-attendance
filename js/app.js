1fcbc8e3c469e547cbc32faff92cb84879f85eb4
zarend <zacharyarend@gmail.com>
2015-07-19 22:03:26 -0700

    programatically generate table

--- js/app.js
+++ js/app.js
@@ -1,84 +1,189 @@
-/* STUDENTS IGNORE THIS FUNCTION
- * All this does is create an initial
- * attendance record if one is not found
- * within localStorage.
- */
-(function() {
-    if (!localStorage.attendance) {
-        console.log('Creating attendance records...');
-        function getRandom() {
-            return (Math.random() >= 0.5);
+$(function() {
+    var Model = (function() {
+        var Model = function() {
+            this.names = [
+                'Slappy the Frog',
+                'Lilly the Lizard',
+                'Paulrus the Walrus',
+                'Gregory the Goat',
+                'Adam the Anaconda'
+            ];
+            this.numDays = 12;
+            if (!localStorage.attendance) {
+                this.genRandomAttendance(this);
+            }
+            this.attendance = JSON.parse(localStorage.attendance);
+            
+            this.missedDays = {};
+            this.calcAllMissedDays();
+        };
+
+        Model.prototype.getNames = function() {
+            return this.names;
         }
 
-        var nameColumns = $('tbody .name-col'),
-            attendance = {};
+        Model.prototype.calcAllMissedDays = function() {
+            var self = this;
+            self.names.forEach(function(nextName) {
+                self.calcMissedDays(nextName);
+            });
+        }
 
-        nameColumns.each(function() {
-            var name = this.innerText;
-            attendance[name] = [];
-
-            for (var i = 0; i <= 11; i++) {
-                attendance[name].push(getRandom());
-            }
-        });
-
-        localStorage.attendance = JSON.stringify(attendance);
-    }
-}());
-
-
-/* STUDENT APPLICATION */
-$(function() {
-    var attendance = JSON.parse(localStorage.attendance),
-        $allMissed = $('tbody .missed-col'),
-        $allCheckboxes = $('tbody input');
-
-    // Count a student's missed days
-    function countMissing() {
-        $allMissed.each(function() {
-            var studentRow = $(this).parent('tr'),
-                dayChecks = $(studentRow).children('td').children('input'),
-                numMissed = 0;
-
-            dayChecks.each(function() {
-                if (!$(this).prop('checked')) {
+        Model.prototype.calcMissedDays = function(name) {
+            numMissed = 0;
+            this.attendance[name].forEach(function(attended) {
+                if (!attended) {
                     numMissed++;
                 }
             });
+            this.missedDays[name] = numMissed;
+        }
 
-            $(this).text(numMissed);
-        });
-    }
+        Model.prototype.genRandomAttendance = function() {
+            var self = this;
+            self.attendance = {};
+            self.names.forEach(function(name) {
+                self.attendance[name] = [];
 
-    // Check boxes, based on attendace records
-    $.each(attendance, function(name, days) {
-        var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
-            dayChecks = $(studentRow).children('.attend-col').children('input');
-
-        dayChecks.each(function(i) {
-            $(this).prop('checked', days[i]);
-        });
-    });
-
-    // When a checkbox is clicked, update localStorage
-    $allCheckboxes.on('click', function() {
-        var studentRows = $('tbody .student'),
-            newAttendance = {};
-
-        studentRows.each(function() {
-            var name = $(this).children('.name-col').text(),
-                $allCheckboxes = $(this).children('td').children('input');
-
-            newAttendance[name] = [];
-
-            $allCheckboxes.each(function() {
-                newAttendance[name].push($(this).prop('checked'));
+                var i;
+                for (i = 1; i <= self.numDays; i++) {
+                    self.attendance[name].push(getRandom());
+                }
             });
-        });
 
-        countMissing();
-        localStorage.attendance = JSON.stringify(newAttendance);
-    });
+            localStorage.attendance = JSON.stringify(self.attendance);
+        }
 
-    countMissing();
+        function getRandom() {
+            return Math.random() >= 0.5;
+        }
+
+        return Model;
+    })();
+
+    var View = (function() {
+        var View = function() {
+            this.$allMissed = $('tbody .missed-col');
+            this.$allCheckboxes = $('tbody input');
+            this.nameColumns = $('tbody .name-col');
+            this.$tableHead = $('#tableHead')[0];
+            this.$tableBody = $('#tableBody')[0];
+        }
+
+        View.prototype.render = function() {
+            var self = this;
+            self.$tableHead.innerHTML = createTableHead(model.numDays);
+
+            var students = ''
+            var name;
+            for (name in model.attendance) {
+                students += createStudentRow(name);
+            }
+
+            self.$tableBody.innerHTML = students;
+
+            self.$allCheckboxes = $('tbody input');
+
+            self.$allCheckboxes.each(function(idx) {
+                var name = model.names[Math.floor(idx / model.numDays)];
+                var col = idx % model.numDays;
+                var $checkBox = self.$allCheckboxes[idx];
+                (function(name, col) {
+                    $checkBox.addEventListener('click', function() {
+                        $checkBox.checked = model.attendance[name][col];
+                    });
+                })(name, col);
+            });
+
+            self.updateChecked();
+        }
+
+        View.prototype.updateChecked = function() {
+            var self = this;
+
+            self.$allCheckboxes.each(function(idx) {
+                var name = model.names[Math.floor(idx / model.numDays)];
+                var col = idx % model.numDays;
+                var $checkBox = self.$allCheckboxes[idx];
+                (function(name, col) {
+                    $checkBox.checked = model.attendance[name][col];
+                })(name, col);
+            });
+        }
+
+        View.prototype.renderMissedDays = function() {
+            $missedDays = $('tbody .missed-col');
+            var i;
+            for (i = 0; i < $missedDays.length; i++) {
+                var thing = $missedDays[i];
+                thing.innerHTML = model.missedDays[model.names[i]];
+            }
+        }
+
+        var createTableHead = function(numDays) {
+            var html = '<tr>\
+                <th class="name-col">Student Name</th>';
+            var col;
+            for (col = 1; col <= numDays; col++) {
+                html += '<th>' + col + '</th>';
+            }
+            html += '<th class="missed-col">Days Missed-col</th>'
+            return html;
+        }
+
+        var createStudentRow = function(name) {
+            var html = '<tr class="student">\
+               <td class="name-col">' + name + '</td>';
+            var col;
+            for (col = 1; col <= model.numDays; col++) {
+                html += '<td class="attend-col"><input type="checkbox"></td>';
+            }
+            html += '<td class="missed-col">' + model.missedDays[name] + '</td>';
+            return html;
+        }
+
+        return View;
+    })();
+
+    var Controller = (function() {
+        var Controller = function() {
+            var self = this;
+
+            console.log('hello from Controller constructor');
+            self.$allCheckboxes = $('tbody input');
+
+            self.addClickHandlers();
+        }
+
+        Controller.prototype.addClickHandlers = function() {
+            var self = this;
+            var names = model.getNames();
+            var numDays = model.numDays;
+
+            self.$allCheckboxes.each(function(idx) {
+                var name = names[Math.floor(idx / numDays)];
+                var col = idx % numDays;
+                var checkBox = self.$allCheckboxes[idx];
+                (function(name, col) {
+                    checkBox.addEventListener('click', function() {
+                        console.log('clicked on ' + name, col);
+                        model.attendance[name][col] = !model.attendance[name][col];
+                        checkBox.checked = model.attendance[name][col];
+                        model.calcMissedDays(name);
+                        view.updateChecked();
+                        view.renderMissedDays();
+                    });   
+                })(name, col);
+            });
+        }
+        return Controller;
+    })();
+
+    model = new Model();
+    view = new View();
+    view.render();
+    
+    handleOnLoad = function() {
+        controller = new Controller();
+    }
 }());
